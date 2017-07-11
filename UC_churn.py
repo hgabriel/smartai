b0be3051538ecf8a7d4c304bcc60f81b6347b4bd
simplified churn and fraud demo code <h2g@datalicht.fritz.box>
2017-07-11 10:11:58 +0200

    no message

--- UC_churn.py
+++ UC_churn.py
@@ -1,60 +1,62 @@
-# pip install keras
-
-import keras
 from keras.models import Sequential
 from keras.layers import Dense
-
-from keras import backend as K
+from keras import backend as k
 import tensorflow as tf
+import numpy as np
 
 
-# prepare input data
-X = []
-y = []
+_input_file = "./inputData/UC_churn_input.csv"
+_output_model_directory = "./models/model_UC_churn/"
+_print_graph_operations = False
 
-with open('./UC_churn_input.csv', 'r') as input_file:
-    for line in input_file.readlines():
-        id,feature_vector,target = line.strip().split('","')
-        id = id[1:]
 
-        vector = []
-        for dimension in feature_vector[1:-1].split(','):
-            vector.append(float(dimension))
-        # vector = [float(dimension) for dimension in feature_vector[1:-1].split(',')]
+def read_csv(file):
+    x_input = []
+    y_input = []
+    with open(file, 'r') as input_file:
+        for line in input_file.readlines()[1:]:
+            feature_vector, target = line.strip().split('","')
+            feature_vector = feature_vector[1:]
+            vector = []
+            for dimension in feature_vector[1:-1].split(','):
+                vector.append(float(dimension))
+            x_input.append(vector)
+            y_input.append(int(target[:-1]))
+    return np.array(x_input), np.array(y_input)
 
-        X.append(vector)
-        y.append(int(target[:-1]))
 
-# build dense network
+def save_model(export_dir, print_graph_operations=False):
+    k.set_learning_phase(0)
+    config = model.get_config()
+    weights = model.get_weights()
+    export_model = Sequential.from_config(config)
+    export_model.set_weights(weights)
+    with k.get_session() as session:
+        input_mapping = {'input': export_model.input}
+        output_mapping = {'output': export_model.output}
+        signature_definition = tf.saved_model.signature_def_utils.predict_signature_def(inputs=input_mapping,
+                                                                                        outputs=output_mapping)
+        signature_definition_map = {tf.saved_model.signature_constants.PREDICT_METHOD_NAME: signature_definition}
+        builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
+        builder.add_meta_graph_and_variables(session, [tf.saved_model.tag_constants.SERVING],
+                                             signature_def_map=signature_definition_map)
+        builder.save(True)
+        if print_graph_operations:
+            print("Graph operations: >>", session.graph.get_operations(), "<<")
+
+
+# read data
+X, Y = read_csv(_input_file)
+
+# create, compile, fit and evaluate model
 model = Sequential()
-model.add(Dense(22, input_dim = 19, activation='relu'))
-model.add(Dense(12, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(30, input_dim=19, activation='relu'))
+model.add(Dense(19, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, Y, epochs=150, batch_size=10)
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'binary_crossentropy'])
-
-model.fit(X, y, epochs=300, batch_size=10)
-
-K.set_learning_phase(0)
-config = model.get_config()
-weights = model.get_weights()
-
-export_model = Sequential.from_config(config)
-export_model.set_weights(weights)
-
-export_dir = './model_demo/'
-
-with K.get_session() as session:
-    input_tensor = tf.get_default_graph().get_tensor_by_name("dense_1_input:0")
-    output_tensor = tf.get_default_graph().get_tensor_by_name("dense_4_target:0")
-
-    input_mapping = {'input':export_model.input}
-    output_mapping = {'output':export_model.output}
-
-    signature_definition = tf.saved_model.signature_def_utils.predict_signature_def(inputs=input_mapping, outputs=output_mapping)
-    signature_definition_map = {tf.saved_model.signature_constants.PREDICT_METHOD_NAME:signature_definition}
-
-    builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
-    builder.add_meta_graph_and_variables(session, [ tf.saved_model.tag_constants.SERVING], signature_def_map= signature_definition_map)
-    builder.save(True)
+# save model
+save_model(_output_model_directory, _print_graph_operations)
--- UC_creditcardfraud.py
+++ UC_creditcardfraud.py
@@ -1,61 +1,62 @@
-# pip install keras
-
-import keras
 from keras.models import Sequential
 from keras.layers import Dense
-
-from keras import backend as K
+from keras import backend as k
 import tensorflow as tf
+import numpy as np
 
 
-# prepare input data
-X = []
-y = []
-
-with open('./UC_creditcardfraud_input.csv', 'r') as input_file:
-    for line in input_file.readlines():
-        feature_vector,target = line.strip().split('","')
-        feature_vector = feature_vector[1:]
-
-        vector = []
-        for dimension in feature_vector[1:-1].split(','):
-            vector.append(float(dimension))
-        # vector = [float(dimension) for dimension in feature_vector[1:-1].split(',')]
-
-        X.append(vector)
-        y.append(int(target[:-1]))
+_input_file = "./inputData/UC_creditcardfraud_input.csv"
+_output_model_directory = "./models/model_UC_creditcardfraud/"
+_print_graph_operations = False
 
 
-# build dense network
+def read_csv(file):
+    x_input = []
+    y_input = []
+    with open(file, 'r') as input_file:
+        for line in input_file.readlines()[1:]:
+            feature_vector, target = line.strip().split('","')
+            feature_vector = feature_vector[1:]
+            vector = []
+            for dimension in feature_vector[1:-1].split(','):
+                vector.append(float(dimension))
+            x_input.append(vector)
+            y_input.append(int(target[:-1]))
+    return np.array(x_input), np.array(y_input)
+
+
+def save_model(export_dir, print_graph_operations=False):
+    k.set_learning_phase(0)
+    config = model.get_config()
+    weights = model.get_weights()
+    export_model = Sequential.from_config(config)
+    export_model.set_weights(weights)
+    with k.get_session() as session:
+        input_mapping = {'input': export_model.input}
+        output_mapping = {'output': export_model.output}
+        signature_definition = tf.saved_model.signature_def_utils.predict_signature_def(inputs=input_mapping,
+                                                                                        outputs=output_mapping)
+        signature_definition_map = {tf.saved_model.signature_constants.PREDICT_METHOD_NAME: signature_definition}
+        builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
+        builder.add_meta_graph_and_variables(session, [tf.saved_model.tag_constants.SERVING],
+                                             signature_def_map=signature_definition_map)
+        builder.save(True)
+        if print_graph_operations:
+            print("Graph operations: >>", session.graph.get_operations(), "<<")
+
+
+# read data
+X, Y = read_csv(_input_file)
+
+# create, compile, fit and evaluate model
 model = Sequential()
-model.add(Dense(35, input_dim = 29, activation='relu'))
-model.add(Dense(18, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(40, input_dim=29, activation='relu'))
+model.add(Dense(30, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, Y, epochs=150, batch_size=100)
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'binary_crossentropy'])
-
-model.fit(X, y, epochs=10, batch_size=10, class_weight={0:1., 1:500})
-
-K.set_learning_phase(0)
-config = model.get_config()
-weights = model.get_weights()
-
-export_model = Sequential.from_config(config)
-export_model.set_weights(weights)
-
-export_dir = './model/'
-
-with K.get_session() as session:
-    input_tensor = tf.get_default_graph().get_tensor_by_name("dense_1_input:0")
-    output_tensor = tf.get_default_graph().get_tensor_by_name("dense_4_target:0")
-
-    input_mapping = {'input':export_model.input}
-    output_mapping = {'output':export_model.output}
-
-    signature_definition = tf.saved_model.signature_def_utils.predict_signature_def(inputs=input_mapping, outputs=output_mapping)
-    signature_definition_map = {tf.saved_model.signature_constants.PREDICT_METHOD_NAME:signature_definition}
-
-    builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
-    builder.add_meta_graph_and_variables(session, [ tf.saved_model.tag_constants.SERVING], signature_def_map= signature_definition_map)
-    builder.save(True)
+# save model
+save_model(_output_model_directory, _print_graph_operations)
